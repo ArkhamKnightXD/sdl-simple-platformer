a0596038308ff6f442c1a0cb2edf3ac8225b0034
knight <karvin.jimenez@alternard.com>
2024-09-20 14:41:21 -0400

    extra changes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "sdl_starter.h"
 #include "sdl_assets_loader.h"
-#include <time.h>
 
 SDL_Window *window = nullptr;
 SDL_Renderer *renderer = nullptr;
@@ -18,30 +17,7 @@ bool isGamePaused;
 SDL_Texture *pauseTexture = nullptr;
 SDL_Rect pauseBounds;
 
-SDL_Texture *scoreTexture = nullptr;
-SDL_Rect scoreBounds;
-
-int score;
-
 TTF_Font *fontSquare = nullptr;
-
-SDL_Rect ball = {SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2, 32, 32};
-
-int ballVelocityX = 400;
-int ballVelocityY = 400;
-
-int colorIndex;
-
-SDL_Color colors[] = {
-    {128, 128, 128, 0}, // gray
-    {255, 255, 255, 0}, // white
-    {255, 0, 0, 0},     // red
-    {0, 255, 0, 0},     // green
-    {0, 0, 255, 0},     // blue
-    {255, 255, 0, 0},   // brown
-    {0, 255, 255, 0},   // cyan
-    {255, 0, 255, 0},   // purple
-};
 
 void quitGame()
 {
@@ -69,7 +45,6 @@ void handleEvents()
             exit(0);
         }
 
-        // To handle key pressed more precise, I use this method for handling pause the game or jumping.
         if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE)
         {
             isGamePaused = !isGamePaused;
@@ -84,16 +59,10 @@ void handleEvents()
     }
 }
 
-int rand_range(int min, int max)
-{
-    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
-}
-
 void update(float deltaTime)
 {
     const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
 
-    // keyboard
     if (currentKeyStates[SDL_SCANCODE_W] && playerSprite.textureBounds.y > 0)
     {
         playerSprite.textureBounds.y -= PLAYER_SPEED * deltaTime;
@@ -114,7 +83,6 @@ void update(float deltaTime)
         playerSprite.textureBounds.x += PLAYER_SPEED * deltaTime;
     }
 
-    // controller
     if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && playerSprite.textureBounds.y > 0)
     {
         playerSprite.textureBounds.y -= PLAYER_SPEED * deltaTime;
@@ -134,39 +102,6 @@ void update(float deltaTime)
     {
         playerSprite.textureBounds.x += PLAYER_SPEED * deltaTime;
     }
-
-    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.w)
-    {
-        ballVelocityX *= -1;
-
-        colorIndex = rand_range(0, 5);
-    }
-
-    else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.h)
-    {
-        ballVelocityY *= -1;
-
-        colorIndex = rand_range(0, 5);
-    }
-
-    else if (SDL_HasIntersection(&playerSprite.textureBounds, &ball))
-    {
-        ballVelocityX *= -1;
-        ballVelocityY *= -1;
-
-        colorIndex = rand_range(0, 5);
-
-        Mix_PlayChannel(-1, actionSound, 0);
-
-        score++;
-
-        std::string scoreString = "score: " + std::to_string(score);
-
-        updateTextureText(scoreTexture, scoreString.c_str(), fontSquare, renderer);
-    }
-
-    ball.x += ballVelocityX * deltaTime;
-    ball.y += ballVelocityY * deltaTime;
 }
 
 void renderSprite(Sprite sprite)
@@ -179,9 +114,7 @@ void render()
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, colors[colorIndex].r, colors[colorIndex].g, colors[colorIndex].b, 255);
-
-    SDL_RenderFillRect(renderer, &ball);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
     renderSprite(playerSprite);
 
@@ -189,11 +122,6 @@ void render()
     {
         SDL_RenderCopy(renderer, pauseTexture, NULL, &pauseBounds);
     }
-
-    SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreBounds.w, &scoreBounds.h);
-    scoreBounds.x = 400;
-    scoreBounds.y = scoreBounds.h / 2;
-    SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreBounds);
 
     SDL_RenderPresent(renderer);
 }
@@ -224,35 +152,25 @@ int main(int argc, char *args[])
         }
     }
 
-    // load font
     fontSquare = TTF_OpenFont("res/fonts/square_sans_serif_7.ttf", 36);
-
-    // load title
-    updateTextureText(scoreTexture, "Score: 0", fontSquare, renderer);
 
     updateTextureText(pauseTexture, "Game Paused", fontSquare, renderer);
 
     SDL_QueryTexture(pauseTexture, NULL, NULL, &pauseBounds.w, &pauseBounds.h);
     pauseBounds.x = SCREEN_WIDTH / 2 - pauseBounds.w / 2;
     pauseBounds.y = 100;
-    // After I use the &pauseBounds.w, &pauseBounds.h in the SDL_QueryTexture.
-    //  I get the width and height of the actual texture
 
     playerSprite = loadSprite(renderer, "res/sprites/alien_1.png", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
     actionSound = loadSound("res/sounds/magic.wav");
 
-    // method to reduce the volume of the sound in half.
     Mix_VolumeChunk(actionSound, MIX_MAX_VOLUME / 2);
 
-    // Load music file (only one data piece, intended for streaming)
     music = loadMusic("res/music/music.wav");
 
-    // reduce music volume in half
     Mix_VolumeMusic(MIX_MAX_VOLUME / 2);
 
-    // Start playing streamed music, put -1 to loop indifinitely
-    Mix_PlayMusic(music, -1);
+    // Mix_PlayMusic(music, -1);
 
     Uint32 previousFrameTime = SDL_GetTicks();
     Uint32 currentFrameTime = previousFrameTime;
@@ -275,6 +193,6 @@ int main(int argc, char *args[])
 
         render();
 
-        // capFrameRate(currentFrameTime);
+        capFrameRate(currentFrameTime);
     }
 }
